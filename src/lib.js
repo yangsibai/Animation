88c38cf8f3c68896e474d00297b90ca0075188ad
Yangsibai <yangsibai@gmail.com>
2015-12-24 22:43:29 +0800

    update animation

--- src/lib.js
+++ src/lib.js
@@ -26,10 +26,11 @@
      * @returns {boolean}
      */
     function hasClass(el, className) {
-        if (el.classList)
-            return el.classList.contains(className)
-        else
-            return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+        if (el.classList) {
+            return el.classList.contains(className);
+        } else {
+            return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+        }
     }
 
     /**
@@ -38,9 +39,11 @@
      * @param className
      */
     function addClass(el, className) {
-        if (el.classList)
-            el.classList.add(className)
-        else if (!hasClass(el, className)) el.className += " " + className
+        if (el.classList) {
+            el.classList.add(className);
+        } else if (!hasClass(el, className)) {
+            el.className += " " + className;
+        }
     }
 
     /**
@@ -49,22 +52,23 @@
      * @param className
      */
     function removeClass(el, className) {
-        if (el.classList)
-            el.classList.remove(className)
+        if (el.classList) {
+            el.classList.remove(className);
+        }
         else if (hasClass(el, className)) {
-            var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
-            el.className = el.className.replace(reg, ' ')
+            var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+            el.className = el.className.replace(reg, ' ');
         }
     }
 
     var CONTAINER_CLASS_NAME = 'animation-stage-container';
-    var ANIMATION_START_CLASS_NAME = 'animation-start';
+    var ANIMATION_START_CLASS_NAME = 'start';
     var CARD_ONE_CLASS_NAME = 'animation-card-one';
     var CARD_TWO_CLASS_NAME = 'animation-card-two';
     var TRANSITION_END_NAME = transitionEndEventName();
 
     /**
-     * animate
+     *
      * @param options
      */
     function animate(options) {
@@ -77,21 +81,26 @@
         addClass(container, CONTAINER_CLASS_NAME);
         addClass(one, CARD_ONE_CLASS_NAME);
         addClass(two, CARD_TWO_CLASS_NAME);
-        var stageAnimationClassName = animateName.split('-').join(' ');
-        addClass(container, stageAnimationClassName);
+        var prefixClass = animateName.split('-').length > 0 ? animateName.split('-')[0] : '';
+        if (prefixClass) {
+            addClass(container, prefixClass);
+        }
+        addClass(container, animateName);
 
         var onTransitionEnd = function () {
             container.removeEventListener(TRANSITION_END_NAME, onTransitionEnd);
             removeClass(container, CONTAINER_CLASS_NAME);
             removeClass(one, CARD_ONE_CLASS_NAME);
             removeClass(two, CARD_TWO_CLASS_NAME);
-            removeClass(container, stageAnimationClassName);
+            if (prefixClass) {
+                removeClass(container, prefixClass);
+            }
+            removeClass(container, animateName);
             removeClass(container, ANIMATION_START_CLASS_NAME);
             onEnd();
         };
 
         container.addEventListener(TRANSITION_END_NAME, onTransitionEnd, false);
-
         window.requestAnimationFrame(function () {
             onStart();
             addClass(container, ANIMATION_START_CLASS_NAME);
