388ffb2e6846b550d8a24e7d0d84542b89c152bc
Yangsibai <yangsibai@gmail.com>
2015-12-24 22:10:59 +0800

    update animate

--- src/lib.js
+++ src/lib.js
@@ -19,64 +19,88 @@
         //TODO: throw 'TransitionEnd event is not supported in this browser';
     }
 
-    function Animation(options) {
-        this.container = options.container;
-        this.one = options.one;
-        this.two = options.two;
-        this.onStart = options.onStart;
-        this.onEnd = options.onEnd;
-        this.originalClassNames = {
-            container: this.container.className,
-            one: this.one.className,
-            two: this.two.className
-        };
-        this.isAnimating = false;
-        this.one.className += ' one';
-        this.two.className += ' two';
-        this.onTransitionEnd = function () {
-            this.isAnimating = false;
-            this.onEnd();
-        }.bind(this);
-        this.container.addEventListener(transitionEndEventName(), this.onTransitionEnd, false);
+    /**
+     * detect if element has a class
+     * @param el
+     * @param className
+     * @returns {boolean}
+     */
+    function hasClass(el, className) {
+        if (el.classList)
+            return el.classList.contains(className)
+        else
+            return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
     }
 
-    Animation.prototype.start = function (animationName) {
-        if (this.isAnimating) {
-            console.warn('is animating');
-            return;
+    /**
+     * add class to an element
+     * @param el
+     * @param className
+     */
+    function addClass(el, className) {
+        if (el.classList)
+            el.classList.add(className)
+        else if (!hasClass(el, className)) el.className += " " + className
+    }
+
+    /**
+     * remove class of an element
+     * @param el
+     * @param className
+     */
+    function removeClass(el, className) {
+        if (el.classList)
+            el.classList.remove(className)
+        else if (hasClass(el, className)) {
+            var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+            el.className = el.className.replace(reg, ' ')
         }
-        if (!animationName) {
-            this.isAnimating = false;
-            this.onEnd();
-            return;
-        }
-        var className = animationName;
-        var arr = animationName.split('-');
-        if (arr.length > 1) {
-            className += " " + arr[0];
-        }
-        this.container.className += ' ' + className;
-        this.isAnimating = true;
+    }
+
+    var CONTAINER_CLASS_NAME = 'animation-stage-container';
+    var ANIMATION_START_CLASS_NAME = 'animation-start';
+    var CARD_ONE_CLASS_NAME = 'animation-card-one';
+    var CARD_TWO_CLASS_NAME = 'animation-card-two';
+    var TRANSITION_END_NAME = transitionEndEventName();
+
+    /**
+     * animate
+     * @param options
+     */
+    function animate(options) {
+        var container = options.container;
+        var one = options.one;
+        var two = options.two;
+        var onStart = options.onStart;
+        var onEnd = options.onEnd;
+        var animateName = options.animate;
+        addClass(container, CONTAINER_CLASS_NAME);
+        addClass(one, CARD_ONE_CLASS_NAME);
+        addClass(two, CARD_TWO_CLASS_NAME);
+        var stageAnimationClassName = animateName.split('-').join(' ');
+        addClass(container, stageAnimationClassName);
+
+        var onTransitionEnd = function () {
+            container.removeEventListener(TRANSITION_END_NAME, onTransitionEnd);
+            removeClass(container, CONTAINER_CLASS_NAME);
+            removeClass(one, CARD_ONE_CLASS_NAME);
+            removeClass(two, CARD_TWO_CLASS_NAME);
+            removeClass(container, stageAnimationClassName);
+            removeClass(container, ANIMATION_START_CLASS_NAME);
+            onEnd();
+        };
+
+        container.addEventListener(TRANSITION_END_NAME, onTransitionEnd, false);
+
         window.requestAnimationFrame(function () {
-            this.onStart();
-            this.container.className += ' start';
-        }.bind(this));
-    };
-
-    Animation.prototype.reset = function () {
-        this.container.className = this.originalClassNames.container;
-        this.one.className = this.originalClassNames.one;
-        this.two.className = this.originalClassNames.two;
-    };
-
-    Animation.prototype.remove = function () {
-        this.reset();
-        this.container.removeEventListener(transitionEndEventName(), this.onTransitionEnd);
-    };
+            onStart();
+            addClass(container, ANIMATION_START_CLASS_NAME);
+        });
+    }
 
     if (global.require && global.module.exports) {
-        module.exports = Animation;
+        global.module.exports.animate = animate;
     } else {
-        global.Animation = Animation;
+        global.animate = animate;
     }
 }(this));
